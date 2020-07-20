11b678b3bf86b09635a61cd7c8c32b7900348dc4
Iretoms <iretomiwajonathan@gmail.com>
2020-07-20 11:10:29 -1100

    Add DELETE/todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,14 @@
+const {ObjectID} = require('mongodb')
+
+const {mongoose} = require('./../server/db/mongoose')
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+
+
+Todo.remove({}).then((todos)=>{
+  console.log(todos);
+})
+
+Todo.findByIdAndRemove('').then((result)=>{
+  console.log(result);
+})
