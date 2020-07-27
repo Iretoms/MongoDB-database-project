fb99dfd59429799d8835297bc1be7ca0b35f87b3
Iretoms <iretomiwajonathan@gmail.com>
2020-07-27 11:49:45 -1100

    Add POST/users/login route

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ app.patch('/todos/:id',(req, res)=>{
 app.post('/users',(req, res)=>{
   let body= _.pick(req.body,['email','password'])
   let user = new User(body)
-     
+
 
     user.generateAuthToken().then((token)=>{
     res.header('x-auth', token).send(user)
@@ -108,6 +108,19 @@ app.post('/users',(req, res)=>{
 app.get('/users/me',authenticate,(req, res)=>{
   res.send(req.user)
 })
+
+app.post('/users/login',(req,res)=>{
+  let body = _.pick(req.body,['email','password'])
+
+  User.findByCredentials(body.email, body.password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e)=>{
+    res.status(400).send()
+  })
+})
+
 
 
 app.listen(3000,()=>{
