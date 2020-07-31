e55513490ccc222adbf95429f02ff0ae4aa46450
Iretoms <iretomiwajonathan@gmail.com>
2020-07-31 12:52:41 -1100

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -11,9 +11,10 @@ let {authenticate} = require('./middleware/authenticate.js')
 let app = express()
 app.use(bodyParser.json())
 
-app.post('/todos',(req, res)=>{
+app.post('/todos',authenticate,(req, res)=>{
   let todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc)=>{
@@ -23,8 +24,10 @@ app.post('/todos',(req, res)=>{
   })
 })
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+  Todo.find({
+   _creator: req.user._id
+ }).then((todos)=>{
     res.send({todos})
   },(e)=>{
     res.status(400).send(e)
@@ -32,13 +35,16 @@ app.get('/todos',(req,res)=>{
 })
 
 
-app.get('/todos/:id',(req, res)=>{
+app.get('/todos/:id',authenticate,(req, res)=>{
   let id = req.params.id
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
-  Todo.findById(id).then((todo)=>{
+  Todo.findOne({
+    _id:id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if (!todo) {
       return res.status(404).send()
     }
@@ -48,13 +54,16 @@ app.get('/todos/:id',(req, res)=>{
   })
 })
 
-app.delete('/todos/:id',(req, res)=>{
+app.delete('/todos/:id',authenticate,(req, res)=>{
   let id = req.params.id
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator: req.user._id
+  }).then((todo)=>{
     if (!todo) {
       return res.status(404).send()
     }
@@ -64,7 +73,7 @@ app.delete('/todos/:id',(req, res)=>{
   })
 })
 
-app.patch('/todos/:id',(req, res)=>{
+app.patch('/todos/:id',authenticate,(req, res)=>{
   let id = req.params.id
   let body = _.pick(req.body,['text','completed'])
 
@@ -79,7 +88,7 @@ app.patch('/todos/:id',(req, res)=>{
     body.completedAt = null
   }
 
-  Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then((todo)=>{
+  Todo.findOneAndUpdate({_id:id,_creator: req.user._id},{$set: body},{new:true}).then((todo)=>{
     if (!todo) {
       return res.status(404).send()
     }
